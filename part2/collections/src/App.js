1193e6fed22484770b08540315a5deabb0cd9c46
Ivan Pugliese <ivan.pugliese@avature.net>
2022-03-03 11:33:03 -0300

    Part2b-Collections-Formularios – Edicio de Form y Filtrado

--- part2/collections/src/App.js
+++ part2/collections/src/App.js
@@ -1,17 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Note from './components/Note'
 
-const App = ({notes}) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("add a new Note ...")
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked - target: ', event.target)
+
+    const noteObject = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+    }
+
+    setNotes(notes.concat(noteObject))
+    setNewNote("add a new Note ...")
+  }
+
+  const handleNoteChange = (event) => {
+    console.log('Note change - target: ', event.target)
+    console.log('Note change - target.value: ', event.target.value)
+
+    setNewNote(event.target.value) 
+  }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
         {
-          notes.map(
+          notesToShow.map(
             note => <Note key={note.id} note={note} />
           )
         }
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
