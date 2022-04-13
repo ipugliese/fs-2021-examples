ec45c0127770bc17acbf935d83109504e8573546
Ivan Pugliese <ivan.pugliese@avature.net>
2022-04-13 16:28:46 -0300

    Part2d-Collections-AlterandoDatosDelServidor – Implementación

--- part2/collections/src/App.js
+++ part2/collections/src/App.js
@@ -1,21 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import Note from './components/Note'
-
 import axios from 'axios'
+
+import Note from './components/Note'
+import noteService from './services/notes.js'
 
 const App = () => {
   const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState("add a new Note ...")
+  const [newNote, setNewNote] = useState("")
   const [showAll, setShowAll] = useState(true)
 
-  const hook = () => {
-    console.log('effect')
-    axios.get('http://localhost:3001/notes').then(response => {
-      console.log('promise fulfilled')
-      setNotes(response.data)
-    })  
-  }
-  useEffect(hook, [])
+  useEffect(() => {
+    noteService.getAll()
+      .then(notes => {
+        setNotes(notes)
+      })
+
+  }, [])
 
   console.log('render', notes.length, 'notes')
 
@@ -24,14 +24,17 @@ const App = () => {
     console.log('button clicked - target: ', event.target)
 
     const noteObject = {
-      id: notes.length + 1,
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
     }
-
-    setNotes(notes.concat(noteObject))
-    setNewNote("add a new Note ...")
+    noteService
+      .create(noteObject)
+      .then(createdNote => {
+        console.log(createdNote)
+        setNotes(notes.concat(createdNote))
+        setNewNote("")
+      })
   }
 
   const handleNoteChange = (event) => {
@@ -43,6 +46,20 @@ const App = () => {
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
 
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(note => note.id === id)
+    noteService
+      .update(id, {...note, important: !note.important})
+      .then(updatedNote => {
+        setNotes(notes.map(note => note.id === id ? updatedNote : note))
+        console.log(`importance of ${id} needs to be toggled`)
+      })
+      .catch(error => {
+        alert(`the note ${note.content} does not exist in the server`)
+        setNotes(notes.filter(note => note.id !== id))
+      })
+  }
+
   return (
     <div>
       <h1>Notes</h1>
@@ -53,8 +70,12 @@ const App = () => {
       </div>
       <ul>
         {
-          notesToShow.map(
-            note => <Note key={note.id} note={note} />
+          notesToShow.map(note => 
+            <Note 
+              key={note.id} 
+              note={note} 
+              toggleImportance={() => toggleImportanceOf(note.id)}
+            />
           )
         }
       </ul>
